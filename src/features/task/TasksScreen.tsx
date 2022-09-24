6b9da18bd90ad2e5d8e41b59509e11df357b208d
Platon Rov <platon.rov@gmail.com>
2022-09-24 17:32:31 +0200

    fix: task list improvements

--- src/features/task/TasksScreen.tsx
+++ src/features/task/TasksScreen.tsx
@@ -1,30 +1,48 @@
-import { IonCheckbox, IonItem, IonLabel, IonList } from "@ionic/react";
+import { IonCheckbox, IonIcon, IonItem, IonLabel, IonList } from "@ionic/react";
+import { starOutline } from "ionicons/icons";
+
+import { importanceToColor, TaskIterationImportanceEnum } from "./store/task-iteration-importance-enum";
+import { TaskIteration } from "./store/task-iteration-store";
 import HomeScreen from "../../components/HomeScreen";
 import { useStore } from "../../store";
-import { importanceToColor, TaskIterationImportanceEnum } from "./store/task-iteration-importance-enum";
 
 import "../../theme/checkbox.css";
 
-const TaskIterationsList = () => {
-    const tasks = useStore((state) => state.taskSlice.tasks);
-    const taskIterations = useStore((state) => state.taskIterationSlice.taskIterations);
+const TaskIterationItem = ({ taskIteration }: { taskIteration: TaskIteration }) => {
     const toggleTaskIteration = useStore((state) => state.taskIterationSlice.toggle);
+    const task = useStore((state) => state.taskSlice.tasks.find((task) => task.id === taskIteration.taskId));
+
+    if (!task) {
+        return null;
+    }
+
+    return (
+        <IonItem key={taskIteration.id}>
+            <IonCheckbox
+                slot="start"
+                checked={taskIteration.completed}
+                onClick={() => toggleTaskIteration(taskIteration.id)}
+                color={importanceToColor(taskIteration.importance)}
+                className={importanceToColor(taskIteration.importance)}
+            ></IonCheckbox>
+
+            <IonLabel>
+                {task.title}
+                {task.description && <p>{task.description}</p>}
+            </IonLabel>
+
+            {task.dreamId && <IonIcon icon={starOutline} slot="end" />}
+        </IonItem>
+    );
+};
+
+const TaskIterationsList = () => {
+    const taskIterations = useStore((state) => state.taskIterationSlice.taskIterations);
 
     return (
         <IonList>
-            {taskIterations.map((taskIteration, index) => (
-                <IonItem key={taskIteration.id}>
-                    <IonCheckbox
-                        slot="start"
-                        checked={taskIteration.completed}
-                        onClick={() => toggleTaskIteration(taskIteration.id)}
-                        color={importanceToColor(taskIteration.importance)}
-                        className={importanceToColor(taskIteration.importance)}
-                    ></IonCheckbox>
-                    <IonLabel>
-                        {tasks.find((task) => task.id === taskIteration.taskId)?.title} {index}
-                    </IonLabel>
-                </IonItem>
+            {taskIterations.map((taskIteration) => (
+                <TaskIterationItem key={taskIteration.id} taskIteration={taskIteration} />
             ))}
         </IonList>
     );
